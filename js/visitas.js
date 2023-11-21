6a248f396247c38a9c63652fbd371611dae93ee3
Sergioig37 <sergioig37@educastur.es>
2023-11-21 09:01:51 +0100

    alguna mejora

--- js/visitas.js
+++ js/visitas.js
@@ -1,64 +1,73 @@
-window.addEventListener("load",init);
+window.addEventListener("load", init);
 
-function comprobarSoporte(){
-    if(typeof(Storage)!=="undefined"){
+function comprobarSoporte() {
+    if (typeof (Storage) !== "undefined") {
         console.info("Tu navegador acepta almacenamiento local");
     }
-    else{
+    else {
         console.info("Tu navegador NO acepta almacenamiento local");
     }
 }
 
-function guardarDatos(clave, valor){
+function guardarDatos(clave, valor) {
     localStorage.setItem(clave, valor);
 }
 
-function comprobarUsuario(){
-    if(localStorage.getItem("usuario")==null){
+function comprobarUsuario() {
+    if (localStorage.getItem("usuario") == null) {
         var usuario = prompt("Mete tu nombre de usuario");
         guardarDatos("usuario", usuario);
         guardarDatos("contador", 0);
+        guardarDatos("visitas", 1);
+    }
+    else {
+        var numeroVisitas = parseInt(recuperarDatos("visitas"));
+        numeroVisitas += 1
+        guardarDatos("visitas", numeroVisitas);
     }
 }
 
 
-function recuperarDatos(clave){
+function recuperarDatos(clave) {
     return localStorage.getItem(clave);
 }
 
-function mandarSaludo(){
-    if(localStorage.getItem("contador")==0){
-    document.getElementById("saludo").innerHTML = "¡Tú primera visita " + localStorage.getItem("usuario")+"!";
-    document.getElementById("contador").innerHTML = "Contador:" +localStorage.getItem("contador");
+function mandarSaludo() {
+
+    switch (parseInt(localStorage.getItem("visitas"))) {
+        case (1):
+            document.getElementById("saludo").innerHTML = "¡Tú primera visita " + localStorage.getItem("usuario") + "!";
+            break;
+        default:
+            document.getElementById("saludo").innerHTML += "Bienvenido de vuelta " + localStorage.getItem("usuario");
+            break;
     }
-    else{
-        document.getElementById("saludo").innerHTML += "Bienvenido de vuelta " + localStorage.getItem("usuario");
-        document.getElementById("contador").innerHTML = "Contador:" +localStorage.getItem("contador");
-    }
-    
+    document.getElementById("contador").innerHTML = "Contador:" + localStorage.getItem("contador");
+
+
 }
 
-function cerrarSesion(){
+function cerrarSesion() {
     localStorage.removeItem("usuario");
 }
 
-function incrementar(){
-    var contadorAu =  parseInt(localStorage.getItem("contador"));
-    contadorAu = contadorAu+1;
+function incrementar() {
+    var contadorAu = parseInt(localStorage.getItem("contador"));
+    contadorAu = contadorAu + 1;
     guardarDatos("contador", contadorAu);
 }
 
-function decrementar(){
-    var contadorAu =  parseInt(localStorage.getItem("contador"));
-    contadorAu = contadorAu-1;
+function decrementar() {
+    var contadorAu = parseInt(localStorage.getItem("contador"));
+    contadorAu = contadorAu - 1;
     guardarDatos("contador", contadorAu);
 }
 
 
-function init(){
-    document.getElementById("logout").addEventListener("click",cerrarSesion,false);
-    document.getElementById("incrementar").addEventListener("click",incrementar,false);
-    document.getElementById("decrementar").addEventListener("click",decrementar,false);
+function init() {
+    document.getElementById("logout").addEventListener("click", cerrarSesion, false);
+    document.getElementById("incrementar").addEventListener("click", incrementar, false);
+    document.getElementById("decrementar").addEventListener("click", decrementar, false);
 }
 
 comprobarUsuario();
